30a90a709b678c6b915095b65d6aa780686618fc
Dorian Buck <dorianbuck@gmail.com>
2021-12-17 19:47:11 +0100

    adds cleanup and refactoring

--- components/Metatags.js
+++ components/Metatags.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 
 export default function Metatags({
-  title = 'The Full Next.js + Firebase Course',
-  description = 'A complete Next.js + Firebase course by Fireship.io',
-  image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
-}) {
+  // title = 'The Full Next.js + Firebase Course',
+  // description = 'A complete Next.js + Firebase course by Fireship.io',
+  // image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
+})
+{
   return (
     <Head>
-      <title>{title}</title>
-      <meta name="twitter:card" content="summary" />
+      {/* <title>{title}</title> */}
+      {/* <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@fireship_dev" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
@@ -16,7 +17,7 @@ export default function Metatags({
 
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={image} /> */}
     </Head>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-// import Head from "next/head";
-// import styles from "../styles/Home.module.css";
-// import Link from "next/link";
-// import toast from "react-hot-toast";
+import { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import toast from "react-hot-toast";
 import Metatags from "../components/Metatags";
 import Loader from "../components/Loader";
 import PostFeed from "../components/PostFeed";
 import { firestore, fromMillis, postToJSON } from "../lib/firebase";
 
-import { useState} from "react";
 
 // Max post to query per page
 const LIMIT = 10;
 
 export async function getServerSideProps(context) {
   const postsQuery = firestore
-    .collectionGroup('posts')
-    .where('published', '==', true)
-    .orderBy('createdAt', 'desc')
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
     .limit(LIMIT);
 
   const posts = (await postsQuery.get()).docs.map(postToJSON);
@@ -37,12 +37,15 @@ export default function Home(props) {
     setLoading(true);
     const last = posts[posts.length - 1];
 
-    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+    const cursor =
+      typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
 
     const query = firestore
-      .collectionGroup('posts')
-      .where('published', '==', true)
-      .orderBy('createdAt', 'desc')
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
       .startAfter(cursor)
       .limit(LIMIT);
 
@@ -58,21 +61,33 @@ export default function Home(props) {
 
   return (
     <main>
-      <Metatags title="Home Page" description="Get the latest posts on our site" />
+      <Metatags
+        title="Home Page"
+        description="Get the latest posts on our site"
+      />
 
       <div className="card card-info">
-        <h2>💡 Next.js + Firebase - The Full Course</h2>
-        <p>Welcome! This app is built with Next.js and Firebase and is loosely inspired by Dev.to.</p>
-        <p>Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content created by other users. All public content is server-rendered and search-engine optimized.</p>
+        <h2>💡 Next.js + Firebase</h2>
+        <p>
+          Welcome! This app is built with Next.js and Firebase and is loosely
+          inspired by Dev.to.
+        </p>
+        <p>
+          Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content
+          created by other users. All public content is server-rendered and
+          search-engine optimized.
+        </p>
       </div>
-     
+
       <PostFeed posts={posts} />
 
-      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
 
       <Loader show={loading} />
 
-      {postsEnd && 'You have reached the end!'}
+      {postsEnd && "You have reached the end!"}
     </main>
   );
 }
