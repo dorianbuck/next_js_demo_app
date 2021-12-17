af4006be99577903ed0210c65f75c63a60c7033c
Dorian Buck <dorianbuck@gmail.com>
2021-12-17 10:56:45 +0100

    adds navbar config

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
+import Loader from "../components/Loader";
+
 export default function Home() {
   return (
     <div>
+      <Loader show={true} />
       <Link
         prefetch={false}
         href={{
